fc726b664865a3c1092ea2244684c26416c13a9a
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-07-14 02:53:25 -0700

    push function

--- stack_func.c
+++ stack_func.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * _push - push int to a stack
  * @stack: linked lists for monty stack
@@ -24,7 +23,6 @@ void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 		(*stack)->prev = top;
 	*stack = top;
 }
-
 /**
  * _pall - print all function
  * @stack: pointer to linked list stack
@@ -41,7 +39,6 @@ void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 		runner = runner->next;
 	}
 }
-
 /**
  * _pint - print int a top of stack
  * @stack: pointer to linked list stack
@@ -59,7 +56,6 @@ void _pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", runner->n);
 }
-
 /**
  * _pop - remove element a list
  * @stack: pointer to first node
@@ -80,7 +76,6 @@ void _pop(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = NULL;
 	free(nodo);
 }
-
 /**
  * free_dlistint - free a list
  * @head: pointer to first node
