2d549d1d9bd2565c230d8186672ee68dae697143
Maurice <mauricejahwizwid@gmail.com>
2022-07-14 12:17:19 +0100

    create add nop and pop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Function useless
+ * @stack: stack structure
+ * @line_number: Number of instructions
+ */
+void nop(UN stack_t **stack, UN unsigned int line_number)
+{
+	;
+}
+Footer
+© 2022 GitHub, Inc.
